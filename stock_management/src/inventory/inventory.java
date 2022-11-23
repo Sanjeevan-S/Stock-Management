41952926319c668324a425feda555211ac89bc6f
Sanjeevan <snjvrocks@gmail.com>
2022-11-23 20:18:54 +0530

    Inventory Package added

--- stock_management/src/inventory/inventory.java
+++ stock_management/src/inventory/inventory.java
@@ -2,6 +2,16 @@ package inventory;
 
 public class inventory {
 	
-	public int noOfcopies;
+	public int total_copies;
+	public int new_stocks;
 	
+	public void addstocks() {
+		total_copies += new_stocks; 
+	}
+	
+	//constructor
+	public inventory(int total_copies,int new_stocks) {
+		this.total_copies = total_copies;
+		this.new_stocks = new_stocks;
+	}
 }
